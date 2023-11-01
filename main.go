40d98ed224e3f8265c28effdd467d381ae741bb4
rodrigo-devpass <123122131+rodrigo-devpass@users.noreply.github.com>
2023-11-01 14:18:36 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 )
 
-func PrintMap() {
-	// Add your solution here!
+func PrintMap(employeeSalaries map[string]int) {
+	for name, salary := range employeeSalaries {
+		fmt.Printf("%s: %d\n", name, salary)
+	}
 }
 
 func main() {
 	employeeSalaries := map[string]int{
-		"John Doe": 50000,
-		"Jane Smith": 60000,
+		"John Doe":    50000,
+		"Jane Smith":  60000,
 		"Mike Johnson": 55000,
 	}
 
